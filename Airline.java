71e69f2e789c873bd713e76920737a6522463d95
Tsholo Kumalo <tsholo.khumalo@ymail.com>
2021-11-05 22:28:19 +0200

    main

--- Airline.java
+++ Airline.java
@@ -4,6 +4,7 @@ public class Airline {
     public int[] seats = new int[10];
     public int i;
     public int choice;
+    public String change;
 
 
     // Reservation
@@ -24,22 +25,44 @@ public class Airline {
     }
 
     public void firstClass() {
+        Scanner in = new Scanner(System.in);
 
         for (i = 0; i < 5; i++) {
             if (seats[i] == 0) {
                 seats[i] = 1;
-                System.out.println("Your seat number is " + i + 1 + " in First Class");
+                System.out.println("Your seat number is " + (i + 1) + " in First Class");
                 break;
                 }
             }
+        if(i>4){
+            System.out.println("First Class is currently at capacity, would you like to be placed in Economy Class? \n Please type Y/N:  ");
+            change = in.next();
+            if(change=="y"){
+                economyClass();
+            }
+            else{
+                System.out.println("Next flight is in 3 hours");
+            }
+        }
 
         }
         public void economyClass(){
+            Scanner in = new Scanner(System.in);
             for (i = 5; i < 10; i++) {
                 if (seats[i] == 0) {
                     seats[i] = 1;
-                    System.out.println("Your seat number is " + i + 1 + " in Economy Class");
+                    System.out.println("Your seat number is " + (i + 1) + " in Economy Class");
                     break;
+                }
+            }
+            if(i>9){
+                System.out.println("Economy Class is currently at capacity, would you like to be placed in First Class? \n Please type Y/N: ");
+                change = in.next();
+                if(change=="y"){
+                    firstClass();
+                }
+                else{
+                    System.out.println("Next flight leaves in 3 hours");
                 }
             }
         }
--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args){
-
+        Airline myAirline = new Airline();
+        while(true) {
+            myAirline.reservation();
+        }
     }
 }
