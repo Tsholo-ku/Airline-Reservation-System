a05c04ef14e2738b4eb417a2fe240fc48f916125
Tsholo Kumalo <tsholo.khumalo@ymail.com>
2021-11-08 10:12:23 +0200

    added boarding pass

--- Main.java
+++ Main.java
@@ -1,8 +1,30 @@
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         Airline myAirline = new Airline();
-        while(true) {
-            myAirline.reservation();
+        while (true) {
+
+            System.out.println("**********MENU**********");
+            System.out.println("Welcome to AIRLINE!!");
+            System.out.println("Please enter your details below");
+            System.out.print("Name: ");
+            String userName = in.next();
+            System.out.print("Surname: ");
+            String userSurname = in.next();
+            System.out.print("Age: ");
+            int userAge = in.nextInt();
+            System.out.print("Destination: ");
+            String userDest = in.next();
+            Passenger myPassenger = new Passenger(userName, userSurname, userAge, userDest);
+            int seatNumber = myAirline.reservation();
+            if(seatNumber!=-1){
+                System.out.println("********BOARDING PASS*******\nPassenger name: "+userName+"\nPassenger Surname: "+userSurname+"\nPasenger age: "+userAge+"\nDestination: "+userDest+"Seat number: "+myAirline.reservation());
+                System.out.println("Your flight is on time");
+                System.out.println("Thank you for using AIRLINE, have a safe trip!!!");
+            }
+
         }
+
     }
 }
--- Passenger.java
+++ Passenger.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Passenger {
+public class Passenger {
+    private String name;
+    private String surname;
+    private int age;
+    private String destination;
+
+    public Passenger(String name, String surname, int age, String destination){
+        this.name = name;
+        this.surname = surname;
+        this.age=age;
+        this.destination=destination;
+    }
+    public String getName(){
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
 }
